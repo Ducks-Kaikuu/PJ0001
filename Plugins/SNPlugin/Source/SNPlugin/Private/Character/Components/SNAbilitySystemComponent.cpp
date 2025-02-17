acc839d513ef00ed30e1bc723b4fbbfec344a0ff
Satoshi_Nishimura <s104nishimura@gmail.com>
2025-02-17 17:46:26 +0900

    ソース整理

--- Plugins/SNPlugin/Source/SNPlugin/Private/Character/Components/SNAbilitySystemComponent.cpp
+++ Plugins/SNPlugin/Source/SNPlugin/Private/Character/Components/SNAbilitySystemComponent.cpp
@@ -3,96 +3,143 @@
 
 #include "Character/Components/SNAbilitySystemComponent.h"
 
+#include "SNDef.h"
 #include "Abilities/Tasks/AbilityTask.h"
 #include "Utility/SNUtility.h"
 #include "Character/AbilitySystem/SNAttributeSet.h"
 
-USNAbilitySystemComponent::USNAbilitySystemComponent(const FObjectInitializer& ObjectInitializer)
-{
+//----------------------------------------------------------------------//
+//
+//! @brief デフォルトコンストラクタ
+//
+//! @param ObjectInitializer イニシャライザ
+//
+//----------------------------------------------------------------------//
+USNAbilitySystemComponent::USNAbilitySystemComponent(const FObjectInitializer& ObjectInitializer){
+	
 	PrimaryComponentTick.bCanEverTick = true;
-
+	
 	PrimaryComponentTick.bStartWithTickEnabled = false;
 }
 
-void USNAbilitySystemComponent::BeginPlay()
-{
+//----------------------------------------------------------------------//
+//
+//! @brief コンポーネントが処理を開始する前にコールされます。
+//
+//----------------------------------------------------------------------//
+void USNAbilitySystemComponent::BeginPlay(){
+	
 	Super::BeginPlay();
-
+	
 	SetComponentTickEnabled(false);
-
+	
 	TArray<FSoftObjectPath> assetList;
-
-	for (auto& Path:AbilityTasks)
-	{
+	
+	for(auto& Path:AbilityClassList){
 		assetList.Add(Path.ToSoftObjectPath());
 	}
-
 	// ロードをリクエスト
 	StreamHandle = SNUtility::RequestAsyncLoad<USNAbilitySystemComponent>(assetList, this, &USNAbilitySystemComponent::LoadedAbility);
 }
 
-void USNAbilitySystemComponent::LoadedAbility()
-{
-	if (StreamHandle.IsValid())
-	{
+//----------------------------------------------------------------------//
+//
+//! @brief BPで設定されているアビリティのロード終了処理
+//
+//----------------------------------------------------------------------//
+void USNAbilitySystemComponent::LoadedAbility(){
+	
+	if(StreamHandle.IsValid()){
+		
 		TArray<UObject*> Assets;
 		
 		StreamHandle->GetLoadedAssets(Assets);
-
-		for (auto& Asset : Assets)
-		{
+		
+		for(auto& Asset : Assets){
+			
 			UClass* Class = Cast<UClass>(Asset);
-
-			if (Class != nullptr)
-			{
-				FGameplayAbilitySpec Spec(FGameplayAbilitySpec(Class->GetDefaultObject<UGameplayAbility>()));
-
-				FGameplayAbilitySpecHandle Handle = GiveAbility(Spec);
-
-				AbilityTaskList.Add(Class, Handle);
+			
+			if(Class != nullptr){
+				AddAbility(Class);
 			}
 		}
 	}
 }
 
-void USNAbilitySystemComponent::AddGameplayTags(const FGameplayTagContainer& Tags)
-{
+//----------------------------------------------------------------------//
+//
+//! @brief GameplayTagCountContainerへゲームプレイタグコンテナを追加
+//
+//! @param Tags 追加するゲームプレイタグコンテナ
+//
+//----------------------------------------------------------------------//
+void USNAbilitySystemComponent::AddGameplayTagsCountContainer(const FGameplayTagContainer& Tags){
 	AddLooseGameplayTags(Tags);
 }
 
-void USNAbilitySystemComponent::RemoveGameplayTags(const FGameplayTagContainer& Tags)
-{
+//----------------------------------------------------------------------//
+//
+//! @brief GameplayTagCountContainerからゲームプレイタグコンテナを削除
+//
+//! @param Tags 削除するゲームプレイタグコンテナ
+//
+//----------------------------------------------------------------------//
+void USNAbilitySystemComponent::RemoveGameplayTagsCountContainer(const FGameplayTagContainer& Tags){
 	RemoveLooseGameplayTags(Tags);
 }
 
-void USNAbilitySystemComponent::RemoveAllGameplayTags()
-{
+//----------------------------------------------------------------------//
+//
+//! @brief GameplayTagCountContainerから全てのゲームプレイタグを削除
+//
+//----------------------------------------------------------------------//
+void USNAbilitySystemComponent::RemoveAllGameplayTagCountContainer(){
 	GameplayTagCountContainer.Reset();
 }
 
-void USNAbilitySystemComponent::AddAbility(UGameplayAbility* AbilityTask)
-{
-	if (AbilityTask != nullptr)
-	{
-		//FGameplayAbilitySpecHandle Handle = GiveAbility(FGameplayAbilitySpec(AbilityTask));
-
-		//AbilityTaskList.Add(AbilityTask, Handle);
+//----------------------------------------------------------------------//
+//
+//! @brief AbilitySystemComponentへアビリティを追加
+//
+//! @param AbilitClass アビリティのクラス情報
+//
+//----------------------------------------------------------------------//
+void USNAbilitySystemComponent::AddAbility(TSubclassOf<UGameplayAbility> AbilitClass){
+	
+	if(AbilitClass != nullptr){
+		
+		FGameplayAbilitySpec Spec(AbilitClass->GetDefaultObject<UGameplayAbility>());
+		
+		if(Spec.Handle.IsValid()){
+			
+			FGameplayAbilitySpecHandle Handle = GiveAbility(Spec);
+			
+			if(Handle.IsValid()){
+				AbilityTaskList.Add(AbilitClass, Handle);
+			} else {
+				SNPLUGIN_ERROR(TEXT("[USNAbilitySystemComponent::AddAbility] : Failed to GiveAbility."));
+			}
+		} else {
+			SNPLUGIN_ERROR(TEXT("[USNAbilitySystemComponent::AddAbility] : Ability Class is Invalidate."));
+		}
 	}
 }
 
-void USNAbilitySystemComponent::RemoveAbility(UGameplayAbility* AbilityTask)
-{
-	if (AbilityTaskList.Contains(AbilityTask->GetClass()) == true)
-	{
-		FGameplayAbilitySpecHandle Handle(AbilityTaskList[AbilityTask->GetClass()]);
-
+//----------------------------------------------------------------------//
+//
+//! @brief AbilitySystemComponentからアビリティを削除
+//
+//! @param AbilityClass アビリティのクラス情報
+//
+//----------------------------------------------------------------------//
+void USNAbilitySystemComponent::RemoveAbility(TSubclassOf<UGameplayAbility> AbilityClass){
+	
+	if(AbilityTaskList.Contains(AbilityClass) == true){
+		
+		FGameplayAbilitySpecHandle Handle(AbilityTaskList[AbilityClass]);
+		
 		SetRemoveAbilityOnEnd(Handle);
-
-		AbilityTaskList.Remove(AbilityTask->GetClass());
+		
+		AbilityTaskList.Remove(AbilityClass);
 	} 
-}
-
-void USNAbilitySystemComponent::SetAttribute(TSubclassOf<USNAttributeSet> Attribute)
-{
-	GetAttributeSubobject(Attribute);
 }
--- Plugins/SNPlugin/Source/SNPlugin/Public/Character/Components/SNAbilitySystemComponent.h
+++ Plugins/SNPlugin/Source/SNPlugin/Public/Character/Components/SNAbilitySystemComponent.h
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
 
@@ -9,48 +9,74 @@
 class USNAttributeSet;
 struct FStreamableHandle;
 class USNAttribute;
-/**
- * 
- */
+
+//!@{@defgroup キャラクター用コンポーネント
+//!@{
+//----------------------------------------------------------------------//
+//
+//! @brief アビリティシステムコンポーネント
+//
+//----------------------------------------------------------------------//
 UCLASS()
 class SNPLUGIN_API USNAbilitySystemComponent : public UAbilitySystemComponent
 {
 	GENERATED_BODY()
 
 public:
-
+	
+	//! @{@name デフォルトコンストラクタ
 	USNAbilitySystemComponent(const FObjectInitializer& ObjectInitializer);
-
+	//! @}
+	
+	//! @{@name GameplayTagCountContainerへゲームプレイタグコンテナを追加
 	UFUNCTION(BlueprintCallable, Category = "SN|AbilitySystem")
-	void AddGameplayTags(const FGameplayTagContainer& Tags);
-
+	void AddGameplayTagsCountContainer(const FGameplayTagContainer& Tags);
+	//! @}
+	
+	//! @{@name GameplayTagCountContainerからゲームプレイタグコンテナを削除
 	UFUNCTION(BlueprintCallable, Category = "SN|AbilitySystem")
-	void RemoveGameplayTags(const FGameplayTagContainer& Tags);
-
+	void RemoveGameplayTagsCountContainer(const FGameplayTagContainer& Tags);
+	//! @}
+	
+	//! @{@name GameplayTagCountContainerから全てのゲームプレイタグを削除
 	UFUNCTION(BlueprintCallable, Category = "SN|AbilitySystem")
-	void RemoveAllGameplayTags();
-
+	void RemoveAllGameplayTagCountContainer();
+	//! @}
+	
+	//! @{@name AbilitySystemComponentへアビリティを追加
 	UFUNCTION(BlueprintCallable, Category = "SN|AbilitySystem")
-	void AddAbility(UGameplayAbility* AbilityTask);
-
+	void AddAbility(TSubclassOf<UGameplayAbility> AbilitClass);
+	//! @}
+	
+	//! @{@name AbilitySystemComponentからアビリティを削除
 	UFUNCTION(BlueprintCallable, Category = "SN|AbilitySystem")
-	void RemoveAbility(UGameplayAbility* AbilityTask);
-
-	void SetAttribute(TSubclassOf<USNAttributeSet> Attribute);
+	void RemoveAbility(TSubclassOf<UGameplayAbility> AbilityClass);
+	//! @}
+	
 protected:
+	
+	//! @{@name コンポーネントが処理を開始する前にコールされます。
 	virtual void BeginPlay() override;
+	//! @}
 	
 private:
-
+	
+	//! @{@name BPで設定されているアビリティのロード終了処理
 	UFUNCTION()
 	void LoadedAbility();
-
-	TSharedPtr<FStreamableHandle> StreamHandle;
-
-	UPROPERTY(EditAnywhere, Category = "SN|AbilitySystem")
-	TArray<TSoftClassPtr<UGameplayAbility>> AbilityTasks;
+	//! @}
 	
+	//! アビリティクラスの非同期ロード用ハンドル
+	TSharedPtr<FStreamableHandle> StreamHandle;
+	
+	//! 親のキャラクターに持たせるアビリティ
+	UPROPERTY(EditAnywhere, Category = "SN|AbilitySystem")
+	TArray<TSoftClassPtr<UGameplayAbility>> AbilityClassList;
+	
+	//! アビリティのクラス情報とハンドルの管理用マップ
+	//!※ネイティブC++の場合はポインタをキーになんてしないように！
 	UPROPERTY()
 	TMap<TObjectPtr<UClass>, FGameplayAbilitySpecHandle> AbilityTaskList;
 };
-
+//! @}
+//! @}
